2dcdf0ef56ba9108470b34a8db9c75b8c7ab4abc
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2023-06-08 12:21:36 +0200

    refactor: minor refactoring in index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             <?php
                 include 'connect.php';
                 define('UPLPATH','images/');
-                $query="SELECT * FROM newsweek WHERE arhiva=0 AND kategorija='us' LIMIT 3";
+                $query="SELECT * FROM newsweek WHERE arhiva=0 AND kategorija='us' LIMIT 4";
                 $result=mysqli_query($dbc,$query);
                     $i=0;
                     while($row=mysqli_fetch_array($result)){
@@ -36,10 +36,11 @@
                             echo '</article>';                    }
             ?>
         <section class="worldSection">
+        <hr class="hrMain">
+        <h2 id="world">World</h2>
         <?php
             include 'connect.php';
-            define('UPLPATH','images/');
-            $query="SELECT * FROM newsweek WHERE arhiva=0 AND kategorija='world' LIMIT 3";
+            $query="SELECT * FROM newsweek WHERE arhiva=0 AND kategorija='world' LIMIT 4";
             $result=mysqli_query($dbc,$query);
                 $i=0;
                 while($row=mysqli_fetch_array($result)){
@@ -56,32 +57,6 @@
                         echo '</div></div>'; 
                         echo '</article>';                    }
         ?>
-            <hr class="hrMain">
-            <h2 id="world">World</h2>
-            <article class="column">
-                <a href="eurovision.html" class="anchorArticle">
-                    <img src="images/eurovision.jpg" alt="eurovision">
-                    <h3>
-                        Eurovision 2019: Israelis and Palestinians Fight to Be Heard
-                    </h3>
-                </a>
-            </article>
-            <article class="column">
-                <a href="navy.html" class="anchorArticle">
-                    <img src="images/navy.jpg" alt="navy">
-                    <h3>
-                        Could U.S. Defeat Irans's Navy? This Exercise Shows War Would Not Be Easy
-                    </h3>
-                </a>
-            </article>
-            <article class="column">
-                <a href="navy.html" class="anchorArticle">
-                    <img src="images/navy.jpg" alt="navy">
-                    <h3>
-                        Could U.S. Defeat Irans's Navy? This Exercise Shows War Would Not Be Easy
-                    </h3>
-                </a>
-            </article>
         </section>
         <footer>
             <?php
