227723d8b065931b165e6fb4239b07d00d9a6d6f
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2023-06-08 12:41:17 +0200

    feat: add administracija.php

---
+++ administracija.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">        
+        <title>Administracija</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <?php
+                include('header.php');
+                ?>
+        </header>
+        <section>
+            
+        </section>
+        <footer>
+            <?php
+                include('footer.php');
+            ?>
+        </footer>
+    </body>
+</hmtl>
