b0088153c5a0538f1727a518bb12ecb9cc78bf91
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2023-06-11 16:23:48 +0200

    refactor: add login logic

--- login.php
+++ login.php
@@ -27,11 +27,11 @@
                 header('Content-Type: text/html; charset=utf-8');
 
                 $servername="127.0.0.1:3307";
-                $username="root";
+                $userServer="root";
                 $password="";
                 $basename="newsweek";
 
-                $con = mysqli_connect($servername, $username, $password, $basename) or die('Error connecting to MYSQL server.'.mysqli_connect_error());
+                $con = mysqli_connect($servername, $userServer, $password, $basename) or die('Error connecting to MYSQL server.'.mysqli_connect_error());
                 mysqli_set_charset($con, "utf8");
                 if($con){
                 // echo "Connected successfully";
@@ -39,31 +39,53 @@
                 //login logic
                 if(isset($_POST['submit'])){
                     $username = $_POST['username'];
-                    $password = $_POST['password'];
+                    $pass = $_POST['password'];
+                    // $pass = password_hash($_POST['password'], CRYPT_BLOWFISH);
+                    $hashed_password=password_hash($pass, CRYPT_BLOWFISH);
+                    
+                    $stmt = $con->prepare("SELECT lozinka FROM korisnik WHERE korisnicko_ime=? LIMIT 1");
+                    $stmt->bind_param('s', $username);
+                    $stmt->execute();
 
-                    $query = "SELECT * FROM korisnik WHERE korisnicko_ime='$username' AND lozinka='$password'";
-                    $result = mysqli_query($con,$query);
-
-                    if(mysqli_num_rows($result)==1){
-                        $_SESSION['username'] = $username;
-                        header('location:index.php');
+                    $isPassOK=false;
+                    $stmt->bind_result($hashFromDB);
+                    if($stmt->fetch()==true){
+                        $isPassOK=password_verify($pass,$hashFromDB);
+                        header("location:index.php");
                     } else {
-                        echo 'Korisnik NE postoji!<br>Morate se prvo <a href="registracija.php">registrirati</a>';
+                        echo 'Korisnik ne postoji!<br><a href="registracija.php">Rgistriraj se</a><br>';
                     }
 
-                }                
+                    // $stmt->bind_result($username, $pass);
+                    // $stmt->store_result();
+                                        
+                    // if($stmt->num_rows()==1){
+                    //     if($stmt->fetch()){
+                    //             echo 'postoji';
+                    //     }
+                    // } else{
+                    //     echo 'nema ga';
+                    // }
+                    // $sql = "SELECT korisnicko_ime, lozinka FROM korisnik WHERE korisnicko_ime=? AND lozinka=? LIMIT 1";
+                    // $stmt = mysqli_stmt_init($con);
+                    
+                    // if(mysqli_stmt_prepare($stmt,$sql)){
+                    //     mysqli_stmt_bind_param($stmt,'ss',$username,$hashed_password);
+                    //     mysqli_stmt_execute($stmt);
+                    //     mysqli_stmt_store_result($stmt);
+                    //     echo $username;
+                    //     mysqli_stmt_bind_result($stmt,$username,$hashed_password);
+                    //     mysqli_stmt_fetch($stmt);
+                    //     echo 'asdasd';
+                    //     if(mysqli_stmt_num_rows($stmt)>0){
+                    //         echo 'uspjesan login<br><br>';
+                    //     } else {
+                    //         echo'';
+                    //     }
+                }
                 $con->close();
             ?>
         </section>
-        <script type="text/javascript">
-            
-            document.getElementById("slanje").onclick =function(event){
-
-                var slanjeForme = true;
-
-                // ime korisnika mora biti uneseno
-            }
-        </script>
             <footer>
                 <?php
                     include('footer.php');
--- registracija.php
+++ registracija.php
@@ -14,14 +14,14 @@
             ?>
         </header>
         <section class="loginSection">
-            <?php
+            <!-- <?php
             // registracija je uspjesno prosla
             if($registriraniKorisnik==true){
                 echo '<p>Korisnik je uspjesno registriran!</p>';
             } else {
                 //registracija nije protekla puspjesno ili je korisnik prvi put dosao na stranicu
             }
-            ?>
+            ?> -->
             <section role="main">
             <form enctype="multipart/form-data" action="" method="post">
                 <div class="form-item">
@@ -43,7 +43,7 @@
 
                     <label for="username">Username:</label>
                     <!-- ispis poruke nakon provjere korisnickog imena u bazi -->
-                    <?php echo '<br><span class="bojaPoruke">'.$msg.'</span>';?>
+                    <!-- <?php echo '<br><span class="bojaPoruke">'.$msg.'</span>';?> -->
                     <div class="form-field">
                         <input type="text" name="username" id="username"class="form-field-textual">
                     </div>
@@ -68,7 +68,117 @@
                     <button type="submit" value="Prijava"id="slanje">Prijava</button>
                 </div>
             </form>
+            <?php
+                $ime=$_POST['ime'];
+                $prezime=$_POST['prezime'];
+                $username=$_POST['username'];
+                $lozinka=$_POST['password'];
+                $hashed_password=password_hash($lozinka, CRYPT_BLOWFISH);
+                $razina=0;
+                $registriraniKorisnik='';
+
+                //provjera postoji li u bazi podataka vec korisnik s tim korisnickim imenom
+                header('Content-Type: text/html; charset=utf-8');
+
+                $servername="127.0.0.1:3307";
+                $userServer="root";
+                $password="";
+                $basename="newsweek";
+
+                $con = mysqli_connect($servername, $userServer, $password, $basename) or die('Error connecting to MYSQL server.'.mysqli_connect_error());
+                mysqli_set_charset($con, "utf8");
+                if($con){
+                echo "Connected successfully";
+                }
+
+                $sql = "SELECT korisnicko_ime FROM korisnik WHERE korisnicko_ime = ?";
+                $stmt = mysqli_stmt_init($con);
+                if(mysqli_stmt_prepare($stmt,$sql)){
+                    mysqli_stmt_bind_param($stmt, 's', $username);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_store_result($stmt);
+                }
+                if(mysqli_stmt_num_rows($stmt)>0){
+                    echo "Korisnicko ime vec postoji!";
+                } else {
+                    // ako ne postoji korisnik s tim usernameom / registracija korisnika u bazi podataka pritom pazeci na SQL injection
+                    $sql="INSERT INTO korisnik(ime, prezime, korisnicko_ime, lozinka, razina) VALUES (?,?,?,?,?)";
+                    $stmt=mysqli_stmt_init($con);
+                    if(mysqli_stmt_prepare($stmt,$sql)){
+                        mysqli_stmt_bind_param($stmt, 'ssssd', $ime, $prezime, $username, $hashed_password, $razina);
+                        mysqli_stmt_execute($stmt);
+                        $registriraniKorisnik=true;
+                    }
+                    header("location:login.php");
+                }
+                mysqli_close($con);
+            ?>
         </section>
+        <script type="text/javascript">
+            
+            document.getElementById("slanje").onclick =function(event){
+
+                var slanjeForme = true;
+
+                // ime korisnika mora biti uneseno
+                var poljeIme =document.getElementById("ime");
+                var ime =document.getElementById("ime").value;
+                if(ime.length == 0){
+                    slanjeForme=false;
+                    poljeIme.style.border="1px dashed red";
+                    document.getElementById("porukaIme").innerHTML="<br>Unesite ime!<br>";
+                } else {
+                    poljeIme.style.border="1px solid green";
+                    document.getElementById("porukaIme").innerHTML="";
+                }
+
+                //prezime korisnika mora biti uneseno
+                var poljePrezime=document.getElementById("prezime");
+                var prezime =document.getElementById("prezime").value;
+                if(prezime.length==0){
+                    slanjeForme=false;
+                    poljePrezime.style.border="1px dashed red";
+                    document.getElementById("porukaPrezime").innerHTML="<br>Unesite prezime!<br>";
+                } else {
+                    poljePrezime.style.border="1px solid green";
+                    document.getElementById("porukaPrezime").innerHTML="";
+                }
+
+                //username mora biti unesen
+                var poljeUsername =document.getElementById("username");
+                var username =document.getElementById("username").value;
+                if(username.length==0){
+                    slanjeForme=false;
+                    poljeUsername.style.border="1p dashed red";
+                    document.getElementById("porukaUsername").innerHTML="<br>Unesite korisnicko ime!<br>";
+                } else {
+                    poljeUsername.style.border="1px solid green";
+                    document.getElementById("porukaUsername").innerHTML="";
+                }
+
+                //provjera podudaranja lozinki
+                var poljePass=document.getElementById("password");
+                var pass =document.getElementById("password").value;
+                var poljePassAgain=document.getElementById("passwordAgain");
+                var passAgain =document.getElementById("passwordAgain").value;
+                if(pass.length==0 || passAgain.length==0 || pass!=passAgain){
+                    slanjeForme=false;
+                    poljePass.style.border="1px dashed red";
+                    poljePassAgain.style.border="1px dashed red";
+                    document.getElementById("porukaPass").innerHTML="<br>Lozinke nisu iste!<br>";
+                    document.getElementById("porukaPassAgain").innerHTML="<br>Lozinke nisu iste!<br>";
+                } else{
+                    poljePass.style.border="1px solid green";
+                    poljePassAgain.style.border="1px solid green";
+                    document.getElementById("porukaPass").innerHTML="";
+                    document.getElementById("porukaPassAgain").innerHTML="";
+                }
+
+                if(slanjeForme!=true){
+                    event.preventDefault();
+                }
+            }
+        </script>
             <footer>
                 <?php
                     include('footer.php');
