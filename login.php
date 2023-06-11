deb96c376d689894c375fb004a45d4efb80765d8
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2023-06-11 16:24:23 +0200

    refactor: minor refactoring

--- login.php
+++ login.php
@@ -40,7 +40,6 @@
                 if(isset($_POST['submit'])){
                     $username = $_POST['username'];
                     $pass = $_POST['password'];
-                    // $pass = password_hash($_POST['password'], CRYPT_BLOWFISH);
                     $hashed_password=password_hash($pass, CRYPT_BLOWFISH);
                     
                     $stmt = $con->prepare("SELECT lozinka FROM korisnik WHERE korisnicko_ime=? LIMIT 1");
@@ -55,33 +54,6 @@
                     } else {
                         echo 'Korisnik ne postoji!<br><a href="registracija.php">Rgistriraj se</a><br>';
                     }
-
-                    // $stmt->bind_result($username, $pass);
-                    // $stmt->store_result();
-                                        
-                    // if($stmt->num_rows()==1){
-                    //     if($stmt->fetch()){
-                    //             echo 'postoji';
-                    //     }
-                    // } else{
-                    //     echo 'nema ga';
-                    // }
-                    // $sql = "SELECT korisnicko_ime, lozinka FROM korisnik WHERE korisnicko_ime=? AND lozinka=? LIMIT 1";
-                    // $stmt = mysqli_stmt_init($con);
-                    
-                    // if(mysqli_stmt_prepare($stmt,$sql)){
-                    //     mysqli_stmt_bind_param($stmt,'ss',$username,$hashed_password);
-                    //     mysqli_stmt_execute($stmt);
-                    //     mysqli_stmt_store_result($stmt);
-                    //     echo $username;
-                    //     mysqli_stmt_bind_result($stmt,$username,$hashed_password);
-                    //     mysqli_stmt_fetch($stmt);
-                    //     echo 'asdasd';
-                    //     if(mysqli_stmt_num_rows($stmt)>0){
-                    //         echo 'uspjesan login<br><br>';
-                    //     } else {
-                    //         echo'';
-                    //     }
                 }
                 $con->close();
             ?>
